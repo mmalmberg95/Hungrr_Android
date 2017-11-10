a3402680d3c1c52847aa9d5b252f5a5efa3630b9
Adam Goldberg <adam.goldberg@drake.edu>
2017-11-10 13:17:48 -0600

    Hi all, Here's what we've got so far. Main activity has two buttons, being the good and bad buttons. Both buttons launch to a new activity called inOutActivity, which is where ultimately the next set of choices for eating in and out would take place. I'm not merging to master yet. Cheers!

--- app/src/main/java/com/drake/android/cs188project3/MainActivity.java
+++ app/src/main/java/com/drake/android/cs188project3/MainActivity.java
@@ -1,13 +1,42 @@
 package com.drake.android.cs188project3;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton good;
+    private ImageButton bad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        good = (ImageButton) findViewById(R.id.goodButton);
+        bad = (ImageButton) findViewById(R.id.badButton);
+
+        good.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), inOutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), inOutActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/drake/android/cs188project3/inOutActivity.java
@@ -0,0 +1,13 @@
+package com.drake.android.cs188project3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_out);
+    }
+}
