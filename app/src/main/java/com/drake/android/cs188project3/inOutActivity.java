2b2d1ca3734b852bdfa2032eccccd82800872601
Adam Goldberg <adam.goldberg@drake.edu>
2017-11-15 13:04:45 -0600

    here is the stuff for arrays. I couldn't get it to log, but what SHOULD be happening is we've got each button mapped assigned a numerical value. each time the button is pressed, the numerical value is added to the array, so different combinations of arrays could be applied as filters. EX: [1,1,2] would be good, fast, cheap.

--- app/src/main/java/com/drake/android/cs188project3/inOutActivity.java
+++ app/src/main/java/com/drake/android/cs188project3/inOutActivity.java
@@ -3,37 +3,53 @@ package com.drake.android.cs188project3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static java.util.stream.IntStream.range;
+
 public class inOutActivity extends AppCompatActivity {
-    private ImageButton good;
-    private ImageButton bad;
+    private ImageButton optOne;
+    private ImageButton optTwo;
+    private ArrayList<Integer> options;
+    //private int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_out);
 
+        optOne = (ImageButton) findViewById(R.id.optOne);
+        optTwo = (ImageButton) findViewById(R.id.optTwo);
+        options = new ArrayList<Integer>();
 
-        good = (ImageButton) findViewById(R.id.goodButton);
-        bad = (ImageButton) findViewById(R.id.badButton);
 
-        good.setOnClickListener(new View.OnClickListener() {
+       // i = 0;
+
+        optOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(getBaseContext(), inOutActivity.class);
-//                startActivity(intent);
+                options.add(1);
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
             }
         });
 
-        bad.setOnClickListener(new View.OnClickListener() {
+        optTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(getBaseContext(), inOutActivity.class);
-//                startActivity(intent);
+                options.add(2);
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
             }
         });
+
 
     }
 }
