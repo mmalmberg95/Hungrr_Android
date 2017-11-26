cacda39501da3d3cec048b3cd38e7774e8210611
mmalmberg95 <30853126+mmalmberg95@users.noreply.github.com>
2017-11-25 20:12:02 -0600

    creation of food class

---
+++ app/src/main/java/com/drake/android/cs188project3/Food.java
@@ -0,0 +1,28 @@
+package com.drake.android.cs188project3;
+
+import android.media.Image;
+
+/**
+ * Created by Matthew on 11/25/2017.
+ */
+
+public class Food {
+    private Image foodImage;
+    private Integer tag;
+
+    public Image getFoodImage() {
+        return foodImage;
+    }
+
+    public void setFoodImage(Image foodImage) {
+        this.foodImage = foodImage;
+    }
+
+    public Integer getTag() {
+        return tag;
+    }
+
+    public void setTag(Integer tag) {
+        this.tag = tag;
+    }
+}
